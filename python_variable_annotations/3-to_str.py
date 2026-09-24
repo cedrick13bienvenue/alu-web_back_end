9bc551b8f7388eabfda5187ed3c3c34ea6639297
bienvenue cedrick <cedrick4smith@gmail.com>
2026-09-24 10:30:45 +0200

    Add type-annotated to_str function

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for basic type-annotated float operations."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a float."""
+    return str(n)

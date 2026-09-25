ebc6d6cd2cbadd045c5114f8db17ba11c08e8cc7
bienvenue cedrick <cedrick4smith@gmail.com>
2026-09-25 11:22:15 +0200

    Add async_generator coroutine

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module for an asynchronous generator."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield 10 random numbers between 0 and 10, one per second."""
     for _ in range(10):
         await asyncio.sleep(1)
